b088270acbb1269f87175fb9aa6e976b3c8d4d78
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-07 18:43:32 +0300

    modify footer

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -7,12 +7,14 @@ import "rxjs";
 })
 export class GithubService {
   private username = "kiprotichdominic";
+  private client_id = "8e26eb4d93a1556cd333";
+  private client_secret = "8321e9fd874a47ca4f74cb135d92cd6feba27a9c";
 
   constructor(private http: HttpClient) {
     console.log("Github service started");
   }
   getUser() {
-    return this.http.get("https://api.github.com/users/" + this.username);
+    return this.http.get("https://api.github.com/users/" + this.username+"?client_id=" );
   }
   getRepos() {
     return this.http.get(
