1aa1874611fa863d65f6968ddf3bf6920468226f
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-09 07:28:22 +0300

    fix token expiry

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -7,19 +7,18 @@ import "rxjs";
 })
 export class GithubService {
   private username = "kiprotichdominic";
-  private client_id = "8e26eb4d93a1556cd333";
-  private client_secret = "8321e9fd874a47ca4f74cb135d92cd6feba27a9c";
+  private client_id = "686b76c425387283e5d6";
+  private client_secret = "dbd3bfbf3c21a2a4aa0945490f9026823f31b62e";
 
   constructor(private http: HttpClient) {
     console.log("Github service started");
   }
   getUser() {
-    return this.http.get("https://api.github.com/users/" + this.username+"?client_id=" );
+    return this.http.get("https://api.github.com/users/" + this.username+"?client_id=" + this.client_id+"&client_secret="+this.client_secret);
   }
   getRepos() {
     return this.http.get(
-      "https://api.github.com/users/" + this.username + "/repos"
-    );
+      "https://api.github.com/users/" + this.username + "/repos"+"?client_id=" + this.client_id+"&client_secret="+this.client_secret);
   }
   updateUsername(username: any) {
     this.username = username;
